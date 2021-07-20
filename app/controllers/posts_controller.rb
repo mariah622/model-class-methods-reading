a56000842a885df0c85ef5fca1b5767a9a8da193
Mariah Morales <mjustine622@gmail.com>
2021-07-20 19:43:07 -0400

    commit tree 6f60cfefba282cc808302aadf12967855322591e

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,8 +1,21 @@
 class PostsController < ApplicationController
+  # helper_method :params
 
   def index
-    @posts = Post.all
-  end
+    @author = Author.all
+
+    if !params[:author].blank?
+      @posts = Post.by_author(params[:author])
+    elsif !params[:date].blank?
+      if params[:date] == "Today"
+        @posts = Post.from_today
+      else
+        @posts = Post.old_news
+      end
+    else
+      # if no filters are applied, show all posts
+      @posts = Post.all
+    end
 
   def show
     @post = Post.find(params[:id])
